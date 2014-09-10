ff590725e554f4c6719a646b0debac669efc139c
rtzarius <rtzarius@gmail.com>
2014-09-10 19:37:27 +1000

    reformat

--- app/app.js
+++ app/app.js
@@ -1,28 +1,28 @@
 'use strict';
 (function () {
 
-  var shapesCtrl = function () {
-    this.foo = 'bar';
-  };
+    var shapesCtrl = function () {
+        this.foo = 'bar';
+    };
 
-  var initFabric = function () {
-    var canvas = new fabric.Canvas('c');
+    var initFabric = function () {
+        var canvas = new fabric.Canvas('c');
 
-// create a rectangle object
-    var rect = new fabric.Rect({
-                                 left  : 100,
-                                 top   : 100,
-                                 fill  : 'red',
-                                 width : 20,
-                                 height: 20
-                               });
+        // create a rectangle object
+        var rect = new fabric.Rect({
+            left: 100,
+            top: 100,
+            fill: 'grey',
+            width: 40,
+            height: 40
+        });
 
-// "add" rectangle onto canvas
-    canvas.add(rect);
-  };
-  initFabric();
+        // "add" rectangle onto canvas
+        canvas.add(rect);
+    };
+    initFabric();
 
-  angular.module('shapeSizer', [])
-    .controller('shapesCtrl', shapesCtrl);
+    angular.module('shapeSizer', [])
+        .controller('shapesCtrl', shapesCtrl);
 
 })();
